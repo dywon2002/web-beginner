5ec6ede332691aacebc0f9b01263918f8f124705
dywon2002 <dywon2002@gmail.com>
2016-01-10 23:09:49 +0900

    homework1 finished

---
+++ week1/homework1.js
@@ -0,0 +1,24 @@
+ 	  function makeDiamond(number){
+
+	   for(var i=1; i<=number;i++)
+	   {
+	    for(var j=number-1; j>=i; j--)
+	     process.stdout.write(" ");
+	    for(var k=1; k<=i*2-1;k++)
+	     process.stdout.write("*");
+	    console.log(" ");
+	   }
+	  
+	   for(var i=1; i<=number-1; i++)
+	   {
+	    for(var j=1; j<=i; j++)
+	     process.stdout.write(" ");
+	    for(var k=2*(number-2)+1; k>=i*2-1;k--)
+	     process.stdout.write("*");
+	    console.log(" ");
+	}
+};
+
+makeDiamond(5);
+makeDiamond(3);
+makeDiamond(4);
